0bf790b05bf0149957cad694ca45be0178d77c34
123DMWM <shmo1joe2@gmail.com>
2013-10-03 20:01:09 -0400

    Added Color Window

---
+++ D3 Classicube Gui/Form2.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D3_Classicube_Gui
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
